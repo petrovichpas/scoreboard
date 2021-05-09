cbff4b6c8976465c4a74f76250e7028c8071c11f
petrovichpas <petrovichpas@gmail.com>
2021-05-09 19:18:23 +0300

    18

--- src/main/java/com/example/scoreboard/entites/HockeyScoreBoard.java
+++ src/main/java/com/example/scoreboard/entites/HockeyScoreBoard.java
@@ -23,35 +23,4 @@ public class HockeyScoreBoard {
     @Column(nullable = false, length = 1)
     private int period;
 
-////  ----------homePenalty---------------
-//    @Column
-//    private String homePenaltyNumber1;
-//    private String homePenaltyNumber2;
-//    private String homePenaltyNumber3;
-//
-//    @Column(length = 1)
-//    private int homePenaltyMinutes1;
-//    private int homePenaltyMinutes2;
-//    private int homePenaltyMinutes3;
-//
-//    @Column(length = 2)
-//    private int homePenaltySeconds1;
-//    private int homePenaltySeconds2;
-//    private int homePenaltySeconds3;
-//
-////  ----------awayPenalty---------------
-//    @Column
-//    private String awayPenaltyNumber1;
-//    private String awayPenaltyNumber2;
-//    private String awayPenaltyNumber3;
-//
-//    @Column(length = 1)
-//    private int awayPenaltyMinutes1;
-//    private int awayPenaltyMinutes2;
-//    private int awayPenaltyMinutes3;
-//
-//    @Column(length = 2)
-//    private int awayPenaltySeconds1;
-//    private int awayPenaltySeconds2;
-//    private int awayPenaltySeconds3;
 }
